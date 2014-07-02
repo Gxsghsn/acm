7cd371f8bd218b320280e6b3e25ecdbe2e1b4f5c
gaoxin <646546851@qq.com>
2014-07-02 17:41:27 +0800

    程序设计入门经典

---
+++ 3-4-6.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    char a[100]="hello, world";
+    char hh[10],mm[10],ss[10];
+//    a = getchar();
+    sscanf("iios/12DDWDFF@122","%*[^/]/%[^D]",&hh);
+//    printf("%d",a);
+    printf("%s\n",hh);
+//    printf("%s\n",mm);
+//    printf("%s\n",ss);
+    return 0;
+}
