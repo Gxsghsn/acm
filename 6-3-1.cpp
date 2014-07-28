c447b8d74b486989f9f5fd4c2e602e88663c09b3
gaoxin <646546851@qq.com>
2014-07-28 21:14:20 +0800

    acm

--- 6-3-1.cpp
+++ 6-3-1.cpp
@@ -4,18 +4,30 @@ using namespace std;
 int treeswith[100];
 int main()
 {
-    int layer,ballnum;
+    int layer,ballnum,ball=0;
+//    while(cin>>layer>>ballnum){
+//        int ball;
+//        memset(treeswith,0,100*sizeof(int));
+//        for(int i=ballnum;i>0;i--){
+//            ball=1;
+//            while(ball*2+1<=pow(2.0,(double)layer)-1){
+//                treeswith[ball-1]=!treeswith[ball-1];
+//                treeswith[ball-1]==1?ball*=2:ball=ball*2+1;
+//            }
+//        }
+//        cout<<ball<<endl;
+//    }
+
+
+
     while(cin>>layer>>ballnum){
-        int ball;
-        memset(treeswith,0,100*sizeof(int));
-        for(int i=ballnum;i>0;i--){
-            ball=1;
-            while(ball*2+1<=pow(2.0,(double)layer)-1){
-                treeswith[ball-1]=!treeswith[ball-1];
-                treeswith[ball-1]==1?ball*=2:ball=ball*2+1;
-            }
+        for(ballnum*=2,ball=0;layer>0;layer--){
+            ball+=(!(ballnum%2))*pow(2.0,(double)layer-1);
+            ballnum-=ballnum/2;
         }
         cout<<ball<<endl;
     }
+
+
     return 0;
 }
