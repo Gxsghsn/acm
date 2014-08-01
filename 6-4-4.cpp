89fd5bb89944ba90147629d550bb82c0e646f3c4
gaoxin <646546851@qq.com>
2014-08-02 00:17:42 +0800

    acm

--- 6-4-4.cpp
+++ 6-4-4.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-int dotnum;
 int maze[100];
 int pathrecord[100][2];
 int pathrecordptr=0;
@@ -11,7 +10,7 @@ bool getdot(string path,int &dot1,int &dot2)
     sscanf(&path[1],"%d",&dot1);
     path.erase(0,path.find(',')+1);
     sscanf(&path[0],"%d",&dot2);
-    cout<<dot1<<dot2<<endl;
+//    cout<<dot1<<dot2<<endl;
     return true;
 }
 
@@ -24,35 +23,44 @@ void record(int dot1,int dot2)
 bool find_euler_path(int start)
 {
     for(int i=0;i<pathrecordptr;i++){
-        if(pathrecord[i][0]==start&&pathrecord[i][1]!=1){
+        if(pathrecord[i][0]==start && pathrecord[i][1]!=1){
             pathrecord[i][1]=1;
+            pathrecord[i+1][1]=1;
 
             int flag=1;
             for(int j=0;j<pathrecordptr;j+=2){
-                if(pathrecord[j]==0)
+                if(pathrecord[j][1]==0)
                     flag=0;
             }
             if(flag==1) return true;
-            start=pathrecord[i+1][0];
-            if(find_euler_path(start)) euler_path_record+=(char)i;
+//            cout<<pathrecordptr<<"  "<<start<<endl;
+            if(i%2==0)
+                start=pathrecord[i+1][0];
+            else
+                start=pathrecord[i-1][0];
+
+            if(find_euler_path(start)) {euler_path_record+=(char)start; return true;}
         }
     }
 }
 
 int main()
 {
+    freopen("6-4-4.txt","r",stdin);
     int pathnum=0;
-    cin>>dotnum;
     string path;
-    while(cin>>path){
+    while(cin>>path&&path!="()"){
         pathnum++;
         int dot1,dot2;
-        if(!getdot(path,dot1,dot2)) break;
+        if(!getdot(path,dot1,dot2)) {break;}
         record(dot1,dot2);
     }
-    if(find_euler_path()){
-        for(int i=euler_path_record.length();i>=00;i++)
-            cout<<euler_path_record[i]<<" ";
+//    for(int i=0;i<pathrecordptr;i++) cout<<pathrecord[i][0]<<" ";
+
+    if(find_euler_path(0)){
+        cout<<"find"<<endl;
+        for(int i=euler_path_record.length();i>=0;i--)
+            cout<<(int)euler_path_record[i]<<"*";
         cout<<endl;
         return 1;
     }
@@ -60,4 +68,5 @@ int main()
         cout<<"No Path"<<endl;
         return 0;
     }
+
 }
