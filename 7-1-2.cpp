7364107cba288dfd9b167c4211209bd452f85749
gaoxin <646546851@qq.com>
2014-08-04 13:17:43 +0800

    acm

---
+++ 7-1-2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long long sum;
+    int num[18];
+    int n=0;
+    while(cin>>num[n]){
+        n++;
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j)
+    }
+
+    return 0;
+}
