fccfbc4f4dd71583634b028f955a62db36eb440d
gaoxin <646546851@qq.com>
2014-08-06 16:28:22 +0800

    acm

--- 7-1-2.cpp
+++ 7-1-2.cpp
@@ -6,32 +6,35 @@ int maxnum(int num[18])
 {
     int maxnum=num[0];
     int maxnumindex=0;
-    for(int i=1;i<18;i++){
+    for(int i=1;abs(num[i]<1000);i++){
         if(abs(num[i])>maxnum){
             maxnum=num[i];
             maxnumindex=i;
         }
     }
     num[maxnumindex]=0;
+    cout<<"maxnum"<<maxnum<<endl;
     return maxnum;
 }
 
 
 int main()
 {
-    long long sum;
-    int num[18];
+    freopen("7-1-2.txt","r",stdin);
+    long long sum=1;
+    int num[18],a;
     int n=0;
     int negative1,negative2;
     while(cin>>num[n]){
         n++;
     }
 
-    for(int i=0;i<18;i++){
-
+    while(a=maxnum(num)||abs(a)>1){
+        cout<<"a "<<a<<endl;
+        sum*=a;
+//        cout<<"sum "<<sum<<endl;
     }
-
-
-
+    if(sum<0) sum*=maxnum(num);
+    cout<<sum<<endl;
     return 0;
 }
