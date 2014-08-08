140428bd4e169095eb5f0880546cc85fe48c7e68
gaoxin <646546851@qq.com>
2014-08-08 20:09:37 +0800

    acm

--- 7-1-2.cpp
+++ 7-1-2.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
+int n=0,num[18];
 
-int maxnum(int num[18])
+int maxnum()
 {
     int maxnum=num[0];
     int maxnumindex=0;
-    for(int i=1;abs(num[i]<1000);i++){
-        if(abs(num[i])>maxnum){
+    for(int i=1;i<n;i++){
+        if(abs(num[i])>abs(maxnum)){
             maxnum=num[i];
             maxnumindex=i;
         }
     }
     num[maxnumindex]=0;
-    cout<<"maxnum"<<maxnum<<endl;
     return maxnum;
 }
 
@@ -22,19 +22,36 @@ int main()
 {
     freopen("7-1-2.txt","r",stdin);
     long long sum=1;
-    int num[18],a;
-    int n=0;
-    int negative1,negative2;
+    int a=1;
+    int negative1=0,negative2;
     while(cin>>num[n]){
         n++;
     }
 
-    while(a=maxnum(num)||abs(a)>1){
-        cout<<"a "<<a<<endl;
-        sum*=a;
-//        cout<<"sum "<<sum<<endl;
+    for(int i=0;i<n;i++) cout<<num[i]<<" ";
+    cout<<endl;
+
+    int b=0;
+    while(b<n){
+        b++;
+        int a=maxnum();
+
+        if(abs(a)<=1) break;
+        if(a<0 && negative1<0) {sum*=(a*negative1);negative1=0;}
+        if(a<0 && negative1==0) {negative1=a;}
+        if(a>0) sum*=a;
     }
-    if(sum<0) sum*=maxnum(num);
+
+    if(negative1<0){
+        while(b<n){
+            b++;
+            int a=maxnum();
+            if(a<0) break;
+        }
+        if(b<n)
+            if(negative1*a>1) sum*=(negative1*a);
+    }
+
     cout<<sum<<endl;
     return 0;
 }
