f82f541bcfe2605d5ede6d7c498d74696b56a93e
gaoxin <646546851@qq.com>
2014-08-11 23:37:37 +0800

    acm

--- 7-2-1.cpp
+++ 7-2-1.cpp
@@ -7,12 +7,17 @@ void dictionary_print(int n,string A,int cur,string B)
         for(int i=0;i<n;i++) cout<<A[i];
         cout<<endl;
     }
-    else for(int j=0;j<n;j++){
+    else for(int j=0;j<n;j++)
+        if(!j||A[j]!=A[j-1]){
         int ok=1;
+        int c1=0,c2=0;
         for(int i=0;i<cur;i++){
-            if(A[i]==B[j]) ok=0;
+            if(A[i]==B[j]) c1++;
         }
-        if(ok){
+        for(int i=0;i<n;i++)
+            if(B[i]==B[j]) c2++;
+//        if(c1<c2) ok=1;
+        if(c1<c2){
             A[cur]=B[j];
             dictionary_print(n,A,cur+1,B);
         }
