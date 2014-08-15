41593865424e4a0052b4f9da69ee54f6e80d80c7
gaoxin <646546851@qq.com>
2014-08-15 19:25:08 +0800

    acm

---
+++ 7-4-3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int n,m;
+char S[100];
+
+int dfs(itn cur)
+{
+    if(cur==n){
+        for(int i=0;i<;i++) cout<<S[i]<<endl;
+        cout<<endl;
+        return 0;
+    }
+    else{
+        for(int i=0;i<m;i++){
+            S[i]=
+        }
+    }
+}
+
+
+
+int main()
+{
+    cin>>n;
+}
