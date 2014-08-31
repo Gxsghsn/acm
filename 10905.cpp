ba43923d714d04f64de8f201f827f1e758eb0ab5
gaoxin <646546851@qq.com>
2014-08-31 14:34:39 +0800

    acm

--- 10905.cpp
+++ 10905.cpp
@@ -1,40 +1,22 @@
 #include<iostream>
-#include<cmath>
+#include<algorithm>
 using namespace std;
-int num[50];
-int non;
 
-//int compare(int maxindex,int weishu)
-//{
-////	int max=
-//	for(int i=0;i<non;i++){
-//		int a=num[i];
-//		int noequal=1;
-//		while(a>=sqrt(10,weishu)) a/=10;
-//		if(a>max){
-//			max=a;
-//			return i;
-//		}
-//		if(a=max){
-//			noequal=0;
-//			compare(max,weishu+1);
-//		}
-//	}
-//}
+bool compare(const string a,const string b)
+{
+    return(a+b>b+a);
+}
 
 int main()
 {
-	// freopen("10905.txt","r",stdin);
-	// cin>>non;
-	// while(non>0){
-	// 	for(int i=0;i<n;i++) cin>>num[i];
-	// 	int max=0;
-	// }
-
-	int n=1234;
-	int a=pow(10.0,3.0);
-	while(n>a) a/=10;
-    while(a>10) a%=10;
-	cout<<a<<endl;
-	return 0;
+    freopen("10905.txt","r",stdin);
+    int n;
+    while(cin>>n&&n){
+        string str[50];
+        for(int i=0;i<n;i++) cin>>str[i];
+        sort(str,str+n,compare);
+        for(int i=0;i<n;i++) cout<<str[i];
+        cout<<endl;
+    }
+    return 0;
 }
