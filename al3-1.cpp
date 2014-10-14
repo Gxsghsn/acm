de2b9029aeef248eca56d9e8ffe2495b1a3bb742
gaoxin <646546851@qq.com>
2014-10-15 00:46:17 +0800

    acm

--- al3-1.cpp
+++ al3-1.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 void matrixMultiply(int **a,int **b,int **c,int ra,int ca,int rb,int cb){
     for(int i=0;i<ra;i++){
-    for(int k=0;k<cb;k++){
-        int sum=0;
-        for(int j=0;j<ca;j++){
-            sum=a[i][j]*b[j][k];
-        }
-        c[i][k]=sum;
+        for(int j=0;j<cb;j++){
+            int sum=0;
+            for(int k=0;k<ca;k++){
+                sum+=(*((int*)a+i*ca+k))*(*((int*)b+k*cb+j));
+            }
+            *((int*)c+i*cb+j)=sum;
         }
     }
 }
@@ -19,7 +19,8 @@ int main()
     int a[2][2]={1,1,2,0};
     int b[2][3]={0,2,3,1,1,2};
     int c[2][3];
-    matrixMultiply(a,b,c,2,2,2,3);
+    matrixMultiply((int **)a,(int **)b,(int **)c,2,2,2,3);
+//    matrixMultiply(a,b,c,2,2,2,3);
     for(int i=0;i<2;i++){
         for(int j=0;j<3;j++)
             cout<<c[i][j]<<" ";
